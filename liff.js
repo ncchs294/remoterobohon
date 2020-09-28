8b773092926f192911642f4ee11c5764ea0c3812
ncchs294 <70068289+ncchs294@users.noreply.github.com>
2020-09-28 15:09:30 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
     var liffId = "1654922011-kRqVLP46";
-    initializeLiff(liffId);
+    //initializeLiff(liffId);
     
     
 })
