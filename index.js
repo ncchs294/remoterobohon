3b05ea7bdf18246b9c04dc3959071243f0b705bf
ncchs294 <70068289+ncchs294@users.noreply.github.com>
2020-10-18 17:37:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -93,8 +93,8 @@ $(function ()
 				datecurrent.getSeconds() + "秒";
         
      //GAS側Postイベントエントリ
-      //var url = 'https://script.google.com/macros/s/AKfycbyZCPemsZEO01buDDevsEWfs8LbvTIIJdGBHMyNi-HW9l9sK4E/exec'
-      var url = 'https://script.google.com/macros/s/AKfycby3tZhehcsDCLsngqFWpKil0gbXaUapzlLESzbgVXNvELXjxa8/exec'
+      var url = 'https://script.google.com/macros/s/AKfycbyZCPemsZEO01buDDevsEWfs8LbvTIIJdGBHMyNi-HW9l9sK4E/exec'
+      //var url = 'https://script.google.com/macros/s/AKfycby3tZhehcsDCLsngqFWpKil0gbXaUapzlLESzbgVXNvELXjxa8/exec'
       
       
       /*
